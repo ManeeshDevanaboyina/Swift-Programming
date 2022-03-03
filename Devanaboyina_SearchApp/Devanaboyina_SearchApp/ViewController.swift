4c7ae3981b3012e5adde3e095a720e40fec94f48
Maneesh Devanaboyina <32562977+ManeeshDevanaboyina@users.noreply.github.com>
2022-03-03 10:53:09 -0600

    Add files via upload

---
+++ Devanaboyina_SearchApp/Devanaboyina_SearchApp/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Devanaboyina_SearchApp
+//
+//  Created by Devanaboyina,Maneesh on 3/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var searchButtonAction: UIButton!
+    
+    @IBOutlet weak var showNextImagesBtn: UIButton!
+    
+    
+    @IBOutlet weak var ShowPrevImagesBtn: UIButton!
+    
+    
+    @IBOutlet weak var topicInfoText: UITextView!
+    
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

892624db33c259373097f45b0a25251d8c781017
Maneesh Devanaboyina <32562977+ManeeshDevanaboyina@users.noreply.github.com>
2022-03-15 10:37:28 -0500

    Add files via upload

---
+++ AnimationDemo/AnimationDemo/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  AnimationDemo
+//
+//  Created by Devanaboyina,Maneesh on 3/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var ImageViewOutlet: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func HappyFunc(_ sender: Any) {
+    }
+    
+    @IBAction func SadFunc(_ sender: Any) {
+        
+    }
+    
+    @IBAction func AngryFunc(_ sender: Any) {
+        
+    }
+    
+    @IBAction func ShakeMeFunc(_ sender: Any) {
+    }
+    
+
+    @IBAction func ShowFunc(_ sender: Any) {
+    }
+    
+    
+    
+}
+

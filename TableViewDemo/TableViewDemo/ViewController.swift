627ccc26bdb27f7703a2113cea2c558c7b4377f6
Maneesh Devanaboyina <maneesh.d123@gmail.com>
2022-03-29 10:26:29 -0500

    updated

--- TableViewDemo/TableViewDemo/ViewController.swift
+++ TableViewDemo/TableViewDemo/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Product{
+    var productName : String?
+    var productCategory : String?
+    
+    init(prodName : String, prodCategory : String){
+        self.productName = prodName
+        self.productCategory = prodCategory
+    }
+}
 
+class ViewController: UIViewController, UITableView, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //return the cell with data
+        
+        <#code#>
+    }
+    
+  
+    
+    
+   
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableViewOutlet.delegate = self
+        tableViewOutlet.delegate = self
     }
-
+    
+    let pr
+    
 
 }
 

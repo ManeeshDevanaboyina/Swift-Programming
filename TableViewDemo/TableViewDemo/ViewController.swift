cd6c525a17f96b341e392d27f7d9466515a6ba63
Maneesh Devanaboyina <maneesh.d123@gmail.com>
2022-03-29 10:01:26 -0500

    created main

---
+++ TableViewDemo/TableViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewDemo
+//
+//  Created by Devanaboyina,Maneesh on 3/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

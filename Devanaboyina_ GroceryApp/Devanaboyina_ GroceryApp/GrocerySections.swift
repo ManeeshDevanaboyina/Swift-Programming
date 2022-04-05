11a8588859bf5750ed4b5effd87e3aa3e860b239
Maneesh Devanaboyina <maneesh.d123@gmail.com>
2022-04-05 10:48:59 -0500

    updated

---
+++ Devanaboyina_ GroceryApp/Devanaboyina_ GroceryApp/GrocerySections.swift
@@ -0,0 +1,19 @@
+//
+//  GrocerySections.swift
+//  Devanaboyina_ GroceryApp
+//
+//  Created by Devanaboyina,Maneesh on 4/5/22.
+//
+
+import Foundation
+
+struct Products {
+    var section : String
+    var items_Array : GroceryItem
+}
+
+struct GroceryItem {
+    var itemName : String
+    var itemImage : String
+    var itemInfo : String
+}

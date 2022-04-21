4c0dbccbc05ae36977b3fd4bbd9fe2dca6b625e7
Maneesh Devanaboyina <maneesh.d123@gmail.com>
2022-04-21 10:24:04 -0500

    updated

--- ContactListApp/ContactListApp/ViewController.swift
+++ ContactListApp/ContactListApp/ViewController.swift
@@ -21,33 +21,65 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
-    var contactNames : [String] = []
+  
 
     
     @IBOutlet weak var tableViewOutlet: UITableView!
     
+    //Initialize an arry of contacts of type String
+    //var contactNames : [String]  = []
+    var contactNames : [String] = []
+    var contacts = NSDictionary()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        tableViewOutlet.delegate=self
-        tableViewOutlet.dataSource=self
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
         fetchData()
         
     }
     
     func fetchData(){
-        //create a database reference
+        //Create the reference
         let databaseRef = Database.database().reference()
         
-        databaseRef.observeSingleEvent(of: .value){
-            snapshot in
-            let contacts = snapshot.value as! NSDictionary
+        //Get the data using the method observeSingleEvent
+        databaseRef.observeSingleEvent(of: .value) { snapshot in
+            self.contacts = snapshot.value as! NSDictionary
+            self.contactNames = self.contacts.allKeys as! [String]
+            self.tableViewOutlet.reloadData();
             
-            self.contactNames = contacts.allKeys as! [String]
+            print(self.contacts)
             
-            //reload the data
+        }
+        
+        
+    }//
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let transistion = segue.identifier
+        if transistion == "contactDetailsSegue"{
+            let destination = segue.destination as! ResultViewController
+            let contactClicked = contactNames[(tableViewOutlet.indexPathForSelectedRow?.row)!]
+            for (key, value) in self.contacts{
+                if key as! String == contactClicked{
+                    for (key1, value1) in value as! [String:Any]{
+                        if key1 == "Email"{
+                            destination.email = value1 as! String
+                        }
+                        else if key1 == "PhoneNum"{
+                            destination.phoneNum = value1 as! Int
+                        }
+                    }
+                }
+                print(key)//String
+                print(value)//Dictionary
+            }
             
-            self.tableViewOutlet.reloadData()
+           
+            
+            
         }
     }
 
